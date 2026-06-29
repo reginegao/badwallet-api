7665af412c091b0de91f049673a99699bd1bce7c
reginegao <foukaregine@gmail.com>
2026-06-29 10:01:30 +0000

    initial commit

---
+++ src/main/java/badwallet_api/BadwalletApiApplication.java
@@ -0,0 +1,13 @@
+package badwallet_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadwalletApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadwalletApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/badwallet_api/BadwalletApiApplicationTests.java
@@ -0,0 +1,13 @@
+package badwallet_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadwalletApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
